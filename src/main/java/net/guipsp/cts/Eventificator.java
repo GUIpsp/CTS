bc422386cebc8170c4aefc1974e2f4c50738303e
Guilherme Espada <porcariadagata@gmail.com>
2012-04-06 22:22:34 +0100

    Remove unneeded event but keep the event class

--- src/main/java/net/guipsp/cts/Eventificator.java
+++ src/main/java/net/guipsp/cts/Eventificator.java
@@ -1,15 +1,9 @@
 package net.guipsp.cts;
 
-import org.bukkit.event.*;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.Listener;
 
 public class Eventificator implements Listener {
 	public Eventificator() {
 	}
 
-	@EventHandler
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		event.getPlayer().sendMessage(
-				"Welcome, " + event.getPlayer().getDisplayName() + "!");
-	}
 }
