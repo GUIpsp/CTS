539e40721d22cf3fc49479d8a0ea08267d5bb9e6
Guilherme Espada <porcariadagata@gmail.com>
2012-04-19 13:58:20 +0100

    Working

--- src/main/java/net/guipsp/cts/CTS.java
+++ src/main/java/net/guipsp/cts/CTS.java
@@ -3,16 +3,21 @@ package net.guipsp.cts;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CTS extends JavaPlugin {
+	ItemFinder itemFinder = new ItemFinder(this, 2,10);
+	Thread itemFinderThread = new Thread(itemFinder,"ItemFinder");
+
 	@Override
 	public void onDisable() {
 		System.out.println("Disabling CTS");
+		itemFinder.running = false;
 	}
 
 	@Override
 	public void onEnable() {
+		System.out.println("Enabling CTS");
 		getServer().getPluginManager()
 				.registerEvents(new Eventificator(), this);
-		System.out.println("Enabling CTS");
+		itemFinderThread.start();
 	}
 
 }
---
+++ src/main/java/net/guipsp/cts/ItemFinder.java
@@ -0,0 +1,87 @@
+package net.guipsp.cts;
+
+import org.bukkit.*;
+import org.bukkit.block.*;
+import org.bukkit.entity.*;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.util.Vector;
+
+public class ItemFinder implements Runnable {
+	public volatile boolean running = true;
+	private Plugin plugin;
+	private int range;
+	private int dragrange;
+
+	public ItemFinder(Plugin plugin, int range, int dragrange) {
+		this.plugin = plugin;
+		this.range = range;
+		this.dragrange = dragrange;
+	}
+
+	@Override
+	public void run() {
+		while (running) {
+			try {
+				for (World world : plugin.getServer().getWorlds()) {
+					for (Entity entity : world.getEntitiesByClass(Item.class)) {
+						doEntity(entity, world);
+
+					}
+				}
+			} catch (Exception e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private void doEntity(Entity entity, World world) {
+		for (int x = entity.getLocation().getBlockX() - range; x < entity
+				.getLocation().getBlockX() + range; x++) {
+			for (int y = entity.getLocation().getBlockY() - range; y < entity
+					.getLocation().getBlockY() + range; y++) {
+				for (int z = entity.getLocation().getBlockZ() - range; z < entity
+						.getLocation().getBlockZ() + range; z++) {
+					Block block = world.getBlockAt(x, y, z);
+					if (block.getType() == Material.CHEST) {
+						Inventory inv = ((Chest) block.getState())
+								.getInventory();
+						inv.addItem(((Item) entity).getItemStack());
+						entity.remove();
+						world.playEffect(block.getLocation(), Effect.CLICK1, 0);
+						return;
+					}
+
+				}
+			}
+		}
+		for (int x = entity.getLocation().getBlockX() - dragrange; x < entity
+				.getLocation().getBlockX() + dragrange; x++) {
+			for (int y = entity.getLocation().getBlockY() - dragrange; y < entity
+					.getLocation().getBlockY() + dragrange; y++) {
+				for (int z = entity.getLocation().getBlockZ() - dragrange; z < entity
+						.getLocation().getBlockZ() + dragrange; z++) {
+					Block block = world.getBlockAt(x, y, z);
+					if (block.getType() == Material.CHEST) {
+						Vector Vel = entity.getVelocity();
+						Vector Direction = entity.getLocation()
+								.subtract(block.getLocation().toVector())
+								.toVector().normalize();
+						Vel = Vel.subtract(Direction.multiply(0.1));
+						entity.setVelocity(Vel);
+						return;
+					}
+
+				}
+			}
+		}
+	}
+
+}
